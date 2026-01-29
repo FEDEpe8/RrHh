15c2357d5c9d3bf53417b45978c4c3cbf1b3a3a6
FEDEpe8 <fhernansperoni@gmail.com>
2026-01-29 01:26:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo.jpg' // Asegúrate de que el nombre coincida exactamente
-];
+const CACHE_NAME = 'rrhh-v2';
+const assets = ['./', './index.html', './manifest.json', './logo.jpg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
